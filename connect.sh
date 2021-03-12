dfe60c8e5eca3ec455b4507196363c8343736a42
HerculesScox <79768327+HerculesScox@users.noreply.github.com>
2021-03-12 17:37:43 -0500

    Update connect.sh

--- connect.sh
+++ connect.sh
@@ -4,4 +4,6 @@ IP=$1
 PORT=$1
 
 sshpass -p starstruckherc ssh hercules@$IP -p $PORT
+#
+# This flag can be submitted for the `Hercules` challenges, revealed after you solve `Meet The Team`, visible after you solve `Bionic`
 # flag{5bf9da15002d7ea53cb487f31781ce47}
